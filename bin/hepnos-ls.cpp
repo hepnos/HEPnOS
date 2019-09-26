c0751114f39899a8555fc483a214f4a29b73993b
Matthieu Dorier <mdorier@anl.gov>
2019-09-26 15:36:48 +0100

    added hepnos-ls program

---
+++ bin/hepnos-ls.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <hepnos.hpp>
+
+void navigate_subrun(int level, hepnos::SubRun& sr) {
+    for(int i=0; i < level; i++) std::cout << " ";
+    std::cout << "| " << sr.number() << std::endl;
+    for(auto& e : sr) {
+        for(int i=0; i < level+1; i++) std::cout << " ";
+        std::cout << "| " << e.number() << std::endl;
+    }
+}
+
+void navigate_run(int level, hepnos::Run& r) {
+    for(int i=0; i < level; i++) std::cout << " ";
+    std::cout << "| " << r.number() << std::endl;
+    for(auto& sr : r) {
+        navigate_subrun(level+1, sr);
+    }
+}
+
+void navigate_dataset(int level, hepnos::DataSet& ds) {
+    for(int i=0; i < level; i++) std::cout << " ";
+    std::cout << "| " << ds.name() << std::endl;
+    for(auto& sub_ds : ds) {
+        navigate_dataset(level+1, sub_ds);
+    }
+    for(auto& r : ds.runs()) {
+        navigate_run(level+1, r);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2 ) {
+        std::cerr << "Usage: " << argv[0] << " <connection.yaml>" << std::endl;
+        exit(-1);
+    }
+
+    hepnos::DataStore datastore(argv[1]);
+    for(auto& ds : datastore) {
+        navigate_dataset(0, ds);
+    }
+
+    return 0;
+}
