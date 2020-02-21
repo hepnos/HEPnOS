4559331a702166af1f26719726f164508775bff1
Matthieu Dorier <mdorier@anl.gov>
2020-02-21 16:40:28 +0000

    added CI files

---
+++ test/ci/jenkins-mcs-workstation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+HERE=`dirname "$0"`
+
+echo "Setting up Spack"
+git clone https://github.com/spack/spack.git
+. spack/share/spack/setup-env.sh
+spack bootstrap
+
+echo "Copying packages.yaml file"
+cp $HERE/mcs-workstation-packages.yaml $HOME/.spack/linux/packages.yaml
+
+echo "Setting up sds-repo"
+git clone https://xgitlab.cels.anl.gov/sds/sds-repo.git
+spack repo add sds-repo
+
+echo "Installing depencencies"
+spack install mpich
+spack install ch-placement
+spack install thallium
+spack install sdskeyval
+spack install yaml-cpp
+spack install boost+serialization
+spack install cmake
+spack install libuuid
+spack install cppunit
+
+echo "List of packages"
+spack find
+
+echo "Loading packages"
+spack load -r mpich
+spack load -r ch-placement
+spack load -r thallium
+spack load -r sdskeyval
+spack load -r yaml-cpp
+spack load -r boost
+spack load -r cmake
+spack load -r libuuid
+spack load -r cppunit
+
+echo "Building HEPnOS"
+mkdir build
+cd build
+cmake .. -DCMAKE_CXX_COMPILER=mpicxx -DENABLE_TESTS=ON -DBUILD_SHARED_LIBS=ON
+make
+make test
