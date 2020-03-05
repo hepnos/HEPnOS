fea13a2a1e94c68adb9d1ebe89a5ca71da34d1d3
Matthieu Dorier <mdorier@anl.gov>
2020-03-05 13:27:31 +0000

    added example 1

--- docs/source/connection.rst
+++ docs/source/connection.rst
@@ -1,2 +1,6 @@
 Client connection
 =================
+
+.. literalinclude:: ../../../examples/01_init_shutdown/main.cpp
+       :language: cpp
+
---
+++ examples/01_init_shutdown/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <hepnos.hpp>
+
+using namespace hepnos;
+
+int main(int argc, char** argv) {
+    
+    if(argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <configfile>" << std::endl;
+		exit(-1)
+	}
+
+    std::string configFile(argv[1]);
+
+    DataStore datastore = DataStore::connect(configFile);
+
+    // ...
+
+    // only if you want to shutdown HEPnOS
+    datastore.shutdown();
+}
