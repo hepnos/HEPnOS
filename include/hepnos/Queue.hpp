e4ac3c39a27c3e1ff6496d3e95bd1c3e75a5b427
Matthieu Dorier <mdorier@anl.gov>
2022-10-12 09:32:13 +0000

    added comments

--- include/hepnos/Queue.hpp
+++ include/hepnos/Queue.hpp
@@ -14,6 +14,10 @@ class QueueImpl;
 class DataStore;
 class DataStoreImpl;
 
+/**
+ * @brief A Queue object is a handle for a queue
+ * of items managed in HEPnOS by a QueueProvider.
+ */
 class Queue {
 
     friend class DataStore;
@@ -21,18 +25,56 @@ class Queue {
 
     public:
 
+    /**
+     * @brief Push an item into the queue.
+     * The type of the item must match the type declared
+     * when opening the queue and the queue must have been
+     * opened with PRODUCER mode.
+     *
+     * @tparam T Type of object
+     * @param object Object to push
+     */
     template<typename T>
     void push(const T& object);
 
+    /**
+     * @brief Check if the queue is currently empty.
+     *
+     * @return Whether the queue is empty.
+     */
     bool empty() const;
 
+    /**
+     * @brief Pop an item from the queue.
+     * If the queue is empty but some processes have
+     * opened it as producer, this function will block
+     * until either a new item appears in the queue or
+     * all the producers have closed the queue.
+     *
+     * The function will return true if an object
+     * was popped, false otherwise.
+     *
+     * @tparam T Type of object
+     * @param object Object to pop into
+     *
+     * @return Whether an object was popped.
+     */
     template<typename T>
     bool pop(T& object);
 
+    /**
+     * @brief Returns the DataStore owning the queue.
+     */
     DataStore datastore() const;
 
+    /**
+     * @brief Close the queue.
+     */
     void close();
 
+    /**
+     * @brief Check that the internal state is valid.
+     */
     bool valid() const;
 
     Queue();
